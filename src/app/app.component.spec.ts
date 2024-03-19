3430767d1664d7f0a1d153417fdef8640bdbfc98
Felipe Garcia <felipe.garcia45@fatec.sp.gov.br>
2024-03-19 17:38:43 -0300

    Pronto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CalculadoraIMC'`, () => {
+  it(`should have as title 'calculadoraimctop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CalculadoraIMC');
+    expect(app.title).toEqual('calculadoraimctop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CalculadoraIMC');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculadoraimctop');
   });
 });
---
+++ src/app/calculo/calculo.component.spec.ts
@@ -0,0 +1,11 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CalculoComponent } from './calculo.component';
+
+describe('CalculoComponent', () => {
+  let component: CalculoComponent;
+  let fixture: ComponentFixture<CalculoComponent>;
+
+  ;
+  });
+;
